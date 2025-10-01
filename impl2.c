8771737fe41b7b182a56e3c39223dc9ab1037ba2
CasioWatch <dropship012@naver.com>
2025-10-01 23:53:24 +0900

    1001 home

--- impl2.c
+++ impl2.c
@@ -7,14 +7,13 @@ typedef struct {
 }CharArrayWrapper;
 
 typedef struct {
-	CharArrayWrapper target;
+	char* array;
 	int cursor;
 	int length;
 }Substring;
 
 typedef struct {
-	Substring strarr[3];
-	int length;
+	Substring array[3];
 }SubstringArrayWrapper;
 
 typedef struct {
@@ -26,39 +25,39 @@ typedef struct {
 int multiStringCompare(SubstringArrayWrapper* Strings1, SubstringArrayWrapper* Strings2) {
 	char array1[48] = {};
 	char array2[48] = {};
-	Substring string1 = {{array1},0,0};
-	Substring string2 = {{array2},0,0};
 	
-	int i,j,cursor;
+	int i,j;
 	
-	for(i=0;i<Strings1->length;i++) {
-		Substring nowString = Strings1->strarr[i];
-		for(j=0;j<Strings1->strarr[i].length;j++) {
-			string1.target.array[string1.length++] = nowString.target.array[nowString.cursor+j];
-		}
-	}
-
-	for(i=0;i<Strings1->length;i++) {
-		Substring nowString = Strings2->strarr[i];
-		for(j=0;j<Strings2->strarr[i].length;j++) {
-			string2.target.array[string2.length++] = nowString.target.array[nowString.cursor+j];
+	int cursor1 = 0;
+	for(i=0;i<3;i++) {
+		Substring nowString = Strings1->array[i];
+		for(j=0;j<nowString.length;j++) {
+			array1[cursor1++] = nowString.array[nowString.cursor+j];
 		}
 	}
 	
+	int cursor2 = 0;
+	for(i=0;i<3;i++) {
+		Substring nowString = Strings2->array[i];
+		for(j=0;j<nowString.length;j++) {
+			array2[cursor2++] = nowString.array[nowString.cursor+j];
+		}
+	}
 	
-	if (string1.length > string2.length) {
+	if (cursor1 > cursor2)
 		return 1;
+	else if(cursor1 < cursor2)
+		return 0;
+	
+	for(i=0;i<48;i++) {
+		if (array1[i] == 0) 
+			return 0;
+		if (array1[i] < array2[i]) 
+			return 1;
+		if (array1[i] > array2[i]) 
+			return 0;
 	}
-	else if (string1.length == string2.length) {
-		for(i=0;i<string1.length;i++) {
-			if (string1.target.array[string1.cursor+i] < string2.target.array[string2.cursor+i]) {
-				return 1;
-			}
-			else if (string1.target.array[string1.cursor+i] > string2.target.array[string2.cursor+i]) {
-				return 0;
-			}
-		}
-	}
+	
 	return 0;
 }
 
@@ -75,9 +74,9 @@ int singleStringCompare(Substring* New, Substring* Proto) {
 	if (New->length == Proto->length) {
 		int i;
 		for(i=0;i<New->length;i++) {
-			if (New->target.array[i] < Proto->target.array[i])
+			if (New->array[i] < Proto->array[i])
 				return 1;
-			if (New->target.array[i] > Proto->target.array[i])
+			if (New->array[i] > Proto->array[i])
 				return 0;
 		}
 	}
@@ -86,21 +85,22 @@ int singleStringCompare(Substring* New, Substring* Proto) {
 }
 
 
+
+
 Substring stringMerge(Substring* F, Substring* R) {
 	Substring result;
 	result.cursor = 0;
 	result.length = F->length + R->length;
 	
-	result.target.length = result.length;
-	result.target.array = (char*)malloc(sizeof(char) * (result.length + 1));
+	result.array = (char*)malloc(sizeof(char) * (result.length + 1));
 	
 	int i;
 	
 	for(i=0;i<F->length;i++) {
-		result.target.array[i] = F->target.array[F->cursor+i];
+		result.array[i] = F->array[F->cursor+i];
 	}
 	for(i=0;i<R->length;i++) {
-		result.target.array[F->length+i] = R->target.array[R->cursor+i];
+		result.array[F->length+i] = R->array[R->cursor+i];
 	}
 	
 	return result;
@@ -110,51 +110,124 @@ Substring stringMerge(Substring* F, Substring* R) {
 
 
 Substring searchAlpha(CharArrayWrapper* InputArray, int GammaCursor, GammaIndex* GammaInfo) {
+	Substring topAlpha = {InputArray->array, 0, 0};
 	
-}
-
-Substring searchBeta(CharArrayWrapper* InputArray, int GammaCursor, GammaIndex* GammaInfo) {
-	int betaCursor = GammaCursor + GammaInfo->rear + 1;
+	int front = GammaCursor + GammaInfo->front;
+	int rear = GammaCursor + GammaInfo->rear;
 	
-	Substring topBeta = {*InputArray, 0, 0};
-	
-	
-	int i,j;
-	for(i=0;i<InputArray->length;i++) {
-		if (
-		(i < GammaCursor || i > betaCursor) &&
-		(InputArray->array[i] == InputArray->array[betaCursor]) ) {//gamma, gamma 바로 뒤 betaCursor 범위 
-			for(j=1; ;j++) {
-				if (
-				!(i+j < GammaCursor || betaCursor+j < InputArray->length) || 
-				!(InputArray->array[i+j] == InputArray->array[betaCursor+j]) )
-					break;
+	if (GammaCursor > 0) {
+		printf("dd\n");
+		int alphaCursor = front - 1;
+		
+		int i,j;
+		for(i=0;i<InputArray->length;i++) {
+			if (
+			(i < alphaCursor || i > rear) &&
+			(InputArray->array[i] == InputArray->array[alphaCursor]) ) {
+				for(j=1; ;j++) {
+					if (
+					!(i - j >= 0) || 
+					!(i - j != rear) || 
+					!(alphaCursor - j >= 0) || 
+					!(alphaCursor - j != i) || 
+					!(InputArray->array[i-j] == InputArray->array[alphaCursor-j]) )
+						break;
+				}
+				
+				Substring nowAlpha = {InputArray->array, i - j + 1, j};
+				
+				if (singleStringCompare(&nowAlpha, &topAlpha)) {
+					topAlpha = nowAlpha;
+				}
+				
+				i += j-1;
 			}
-			
-			Substring nowBeta = {*InputArray, i, j};
-			
-			if (singleStringCompare(&nowBeta,&topBeta)) {
-				topBeta = nowBeta;
+		}
+		
+		if (topAlpha.length != 0) 
+			return topAlpha;
+	}
+	
+	
+	//최대 길이 gamma에서 검색 실패 시 gamma 길이 줄이며 검색 시도 
+	while(GammaInfo->front < GammaInfo->rear) {
+		GammaInfo->front++;
+		front = GammaCursor + GammaInfo->front;
+		rear = GammaCursor + GammaInfo->rear;
+		
+		int nowAlphaCursor = front-1;
+		
+		int j;
+		for(j=0;j<InputArray->length;j++) {
+			if (
+			(j < nowAlphaCursor || j > rear) && 
+			InputArray->array[j] == InputArray->array[nowAlphaCursor]) {
+				topAlpha.cursor = j;
+				topAlpha.length = 1;
+				
+				return topAlpha;
 			}
 		}
 	}
 	
-	if (topBeta.length != 0)
-		return topBeta;
+	return topAlpha;
+}
+
+Substring searchBeta(CharArrayWrapper* InputArray, int GammaCursor, GammaIndex* GammaInfo) {
+	Substring topBeta = {InputArray->array, 0, 0};
+	
+	int front = GammaCursor + GammaInfo->front;
+	int rear = GammaCursor + GammaInfo->rear;
+	
+	if (rear + 1 < InputArray->length) {
+		int betaCursor = rear + 1;
+		
+		int i;
+		for(i=0;i<InputArray->length;i++) {
+			if (
+			(i < front || i > betaCursor) &&
+			(InputArray->array[i] == InputArray->array[betaCursor]) ) {//gamma, gamma 바로 뒤 betaCursor 범위 
+				int j=0;
+				for(j=1; ;j++) {
+					if (
+					!(i + j < InputArray->length) || 
+					!(i + j != front) || 
+					!(betaCursor + j < InputArray->length) || 
+					!(betaCursor + j != i) || 
+					!(InputArray->array[i+j] == InputArray->array[betaCursor+j]) )
+					break;
+				}
+				Substring nowBeta = {InputArray->array, i, j};
+				
+				if (singleStringCompare(&nowBeta, &topBeta)) {
+					topBeta = nowBeta;
+				}
+				
+				i += j-1;
+			}
+		}
+		
+		if (topBeta.length != 0)
+			return topBeta;
+	}
+	
 	
 	//최대 길이 gamma에서 검색 실패 시 gamma 길이 줄이며 검색 시도 
-	while(GammaInfo->rear-- > GammaInfo->front){
-		int front = GammaCursor + GammaInfo->front;
-		int rear = GammaCursor + GammaInfo->rear;
+	while(GammaInfo->rear > GammaInfo->front) {
+		GammaInfo->rear--;
+		front = GammaCursor + GammaInfo->front;
+		rear = GammaCursor + GammaInfo->rear;
 		
 		int nowBetaCursor = rear + 1;
 		
+		int j;
 		for(j=0;j<InputArray->length;j++) {
 			if (
 			(j < front || j > nowBetaCursor) &&
 			InputArray->array[j] == InputArray->array[nowBetaCursor]) {
 				topBeta.cursor = j;
 				topBeta.length = 1;
+				
 				return topBeta;
 			}
 		}
@@ -162,13 +235,16 @@ Substring searchBeta(CharArrayWrapper* InputArray, int GammaCursor, GammaIndex* 
 	
 	return topBeta;
 }
-Substring searchMain(CharArrayWrapper* InputArray1, CharArrayWrapper* InputArray2) {
+
+
+SubstringArrayWrapper searchMain(CharArrayWrapper* InputArray1, CharArrayWrapper* InputArray2) {
+	SubstringArrayWrapper top = {};
+	
 	int i,j,k;
 	for(i=1;i<InputArray1->length;i++) {
 		for(j=0;j<InputArray2->length-1;j++) {
-			if (InputArray1->array[i] == InputArray2->array[j] && 
-			((i == 0 || j == 0) || InputArray1->array[i-1] == InputArray2->array[j-1])) {
-				//alpha, beta 검색 로직 
+			if (InputArray1->array[i] == InputArray2->array[j] /*&& 
+			((i == 0 || j == 0) || InputArray1->array[i-1] != InputArray2->array[j-1])*/) {
 				for(k=1; ;k++) {
 					if (
 					 !(i+k < InputArray1->length) ||
@@ -176,38 +252,62 @@ Substring searchMain(CharArrayWrapper* InputArray1, CharArrayWrapper* InputArray
 					 !(InputArray1->array[i+k] == InputArray2->array[j+k]) )
 						break;
 				}
-				
+								
 				GammaIndex gammaIndex = {0,k-1};
+				printf("\n%d %d\n", gammaIndex.front, gammaIndex.rear);
+				Substring nowAlpha = searchAlpha(InputArray1, i, &gammaIndex);
+				printf("%d %d\n", gammaIndex.front, gammaIndex.rear);
+				Substring nowBeta = searchBeta(InputArray2, j, &gammaIndex);
+				printf("%d:%d %d, %d %d\n", i, gammaIndex.front, gammaIndex.rear, nowAlpha.length, nowBeta.length);
 				
-				Substring alpha = searchAlpha(InputArray1, i, &gammaIndex);
-				Substring beta = searchBeta(InputArray2, j, &gammaIndex);
+				Substring nowGamma = {InputArray1->array, i+gammaIndex.front, gammaIndex.rear-gammaIndex.front+1};
 				
+				SubstringArrayWrapper wrapper = {{nowAlpha, nowGamma, nowBeta}};
+				printf("%d %d %d\n", nowAlpha.length, nowGamma.length, nowBeta.length);
+				
+				if (
+				nowAlpha.length > 0 && 
+				nowBeta.length > 0 &&
+				multiStringCompare(&wrapper, &top) ) {
+					top = wrapper;
+					
+					int a,b;
+					for(a=0;a<3;a++) {
+						Substring nowString = top.array[a];
+						for(b=0;b<nowString.length;b++) {
+							printf("%c", nowString.array[nowString.cursor+b]);
+						}
+						printf(" ");
+					}
+					
+					printf("\n");
+				}
 			}
 		}
 	}
+	
+	return top;
 }
 int main(void) {
-//	char a1[5];
-//	char a2[5];
-//	char a3[5];
-//	char a4[5];
-//	scanf("%s",a1);
-//	scanf("%s",a2);
-//	scanf("%s",a3);
-//	scanf("%s",a4);
-//	SubstringArrayWrapper wrapper1 = {{{{a1, 4}, 0, 4}, {{a2, 4}, 0, 4}}, 2};
-//	SubstringArrayWrapper wrapper2 = {{{{a3, 4}, 0, 4}, {{a4, 4}, 0, 4}}, 2};
-	char InputArray[48] = {};
-	scanf("%s", InputArray);
-	int i;
-	for(i=0;InputArray[i];i++);
+	char inputArray1[48] = {};
+	char inputArray2[48] = {};
 	
-	CharArrayWrapper wrapper = {InputArray, i};
+	scanf("%s %s", inputArray1, inputArray2);
 	
-	GammaIndex idx = {0,2};
-	Substring a = searchBeta(&wrapper, 2, &idx);
+	int i,j;
+	for(i=0;inputArray1[i];i++);
+	CharArrayWrapper input1 = {inputArray1, i};
+	for(i=0;inputArray2[i];i++);
+	CharArrayWrapper input2 = {inputArray2, i};
 	
-	for(i=0;i<a.length;i++)
-		printf("%c", a.target.array[a.cursor+i]);
+	SubstringArrayWrapper result = searchMain(&input1, &input2);
+	
+	for(i=0;i<3;i++) {
+		Substring nowString = result.array[i];
+		for(j=0;j<nowString.length;j++) {
+			printf("%c", nowString.array[nowString.cursor+j]);
+		}
+	}
+	
 	return 0;
 }
